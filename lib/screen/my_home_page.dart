bbc674309f729ad3ed29ae7a18de2a0f4d0b71d2
oown-mac <oown.natthpon@icloud.com>
2024-05-16 16:33:35 +0700

    button plus +

---
+++ lib/screen/my_home_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('my increment'),
+      ),
+      body: Center(child: Text("นับไปแล้ว $_counter")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
